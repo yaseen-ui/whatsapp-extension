7b57f09da01bbf6f421cfce4d1c342af7812946d
yaseen-ui <68981524+yaseen-ui@users.noreply.github.com>
2023-10-25 21:55:15 +0530

    WEP: added file upload

--- app/contentScript.js
+++ app/contentScript.js
@@ -25,6 +25,21 @@ const sendDataToAPI = (API_NAME = '', payLoad = {}, method = 'POST') => {
             console.error(error); // Example: Logging the error to the console 
         });
 }
+
+const bs64ToBlob = (base64String) => {
+    const matches = base64String.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+    const contentType = matches[1];
+    const base64Data = matches[2];
+    const byteCharacters = atob(base64Data);
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+        byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    const byteArray = new Uint8Array(byteNumbers);
+    blob = new Blob([byteArray], { type: contentType });
+    return blob;
+}
+
 chrome.runtime.onMessage.addListener((response, sender) => {
     if (response.tabOpen) {
         sendContactsList();
@@ -36,13 +51,19 @@ chrome.runtime.onMessage.addListener((response, sender) => {
         // textBox.innerHTML = message;
         // textBox.dispatchEvent(event);
         // document.getElementsByClassName("_2xy_p epia9gcq")[0].click();
-        const payLoad = {
-            "messaging_product": "whatsapp",
-            "recipient_type": "individual",
-            "to": response.number,
-            "type": "text",
-            "text": response.text
+        let image;
+        if (response.bs64Img) {
+            image = bs64ToBlob(response.bs64Img);
         }
+        const payLoad = {
+            messaging_product: "whatsapp",
+            recipient_type: "individual",
+            to: response.number,
+            type: image ? 'image' : "text",
+            text: response.text,
+            image
+        }
+        console.log('pela', payLoad);
         sendDataToAPI(`https://graph.facebook.com/v18.0/${response.number}/messages`, payLoad);
     }
 })
--- app/popup.js
+++ app/popup.js
@@ -1,9 +1,36 @@
 const btn = document.getElementById('send');
 
+let bs64Img = '';
+
+const imageToBlobObject = () => {
+    const fileInput = document.getElementById('fileInput');
+    fileInput.addEventListener('change', function (event) {
+      if (event.target.files.length > 0) {
+        const selectedFile = event.target.files[0];
+        const reader = new FileReader();
+        reader.onload = function (event) {
+            bs64Img = event.target.result;
+        };
+        reader.readAsDataURL(selectedFile);
+      }
+    });
+  }
+
+const toggleUpload = (flag) => {
+    btn.disabled = flag;
+    if(flag) {
+        document.getElementById('uploadLabel').classList.remove("hidden")
+    } else {
+        document.getElementById('uploadLabel').classList.add("hidden")
+    }
+}
+
+imageToBlobObject();
+
 btn.addEventListener('click', () => {
     const text = document.getElementById("wtName").value;
     const number = document.getElementById("contactsDD").value;
-    const message = { text, number };
+    const message = { text, number,  bs64Img};
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         chrome.tabs.sendMessage(tabs[0].id, message)
     })
@@ -15,10 +42,10 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
 
 chrome.runtime.onMessage.addListener(function (response, sender, sendResponse) {
     console.log("Message received in popup.js:", response);
-    message = response.message.map(ele=>`+${ele.substring(0,12)}`);
+    message = response.message.map(ele => { return { label: `+${ele.substring(0, 12)}`, value: ele } });
     let html = '<option>Select Number(s)</option>';
     message.forEach(element => {
-        html += `<option value=${element}>${element}</option>`;
+        html += `<option value=${element.value}>${element.label}</option>`;
     });
     document.getElementById('contactsDD').innerHTML = html;
 });
